93e71ab3bc012e8eb13e6567fe2cf873f505fcbd
Zeeshan Rasool <zrasool88@gmail.com>
2015-05-06 13:14:32 +0100

    the awesome people at stackoverflow helped me see the mistake with my recursive method. it works now!

--- lib/array.rb
+++ lib/array.rb
@@ -1,37 +1,14 @@
 class Array
-  def mymap(&block)
+  def mymap
     new_array = []
-    map_old_array_to_new(new_array, &block)
-  end
-
-  def mymap_recursive(&block)
-    copy_of_array = self.dup
-    new_array = []
-    run_it_recursively(copy_of_array, new_array, &block)
-  end
-
-  private
-
-  def map_old_array_to_new(new_array, &block)
-    self.each do |element|
-      value = yield(element)
-      new_array.push(value)
-    end
+    each { |element| new_array << yield(element) }
     new_array
   end
 
-  def run_it_recursively(copy_of_array, new_array, &block)
-    # puts "1. #{new_array}"
+  def mymap_recursive(new_array = [], &block)
+    copy_of_array = dup
     return new_array if copy_of_array.empty?
-    # puts "2. #{new_array}"
-    new_value = yield(copy_of_array[0])
-    # puts "3. #{new_array}"
-    new_array.push(new_value)
-    # puts "4. #{new_array}"
-    copy_of_array.drop(1).mymap_recursive(&block)
-    puts "5. #{new_array}"
+    new_array << yield(copy_of_array.shift)
+    copy_of_array.mymap_recursive(new_array, &block)
   end
 end
-
-arr = [2,2,5,5,10]
-arr.mymap_recursive {|n| n * n}
--- spec/array_recursive_spec.rb
+++ spec/array_recursive_spec.rb
@@ -4,37 +4,39 @@ require 'array'
 describe Array do
   let(:arr) { [2, 2, 5, 5, 10] }
 
-  context 'arithmetic operations' do
-    it 'can run addition on numbers' do
-      expect(arr.mymap_recursive { |number| number + 2 }).to eq([4, 4, 7, 7, 12])
+  context 'recursive map function' do
+    context 'arithmetic operations' do
+      it 'can run addition on numbers' do
+        expect(arr.mymap_recursive { |number| number + 2 }).to eq([4, 4, 7, 7, 12])
+      end
+
+      it 'can run subtraction on numbers' do
+        expect(arr.mymap_recursive { |number| number - 2 }).to eq([0, 0, 3, 3, 8])
+      end
+
+      it 'can run multiplication on numbers' do
+        expect(arr.mymap_recursive { |number| number * 2 }).to eq([4, 4, 10, 10, 20])
+      end
+
+      it 'can run division on numbers' do
+        expect(arr.mymap_recursive { |number| number / 2 }).to eq([1, 1, 2, 2, 5])
+      end
     end
 
-    it 'can run subtraction on numbers' do
-      expect(arr.mymap_recursive { |number| number - 2 }).to eq([0, 0, 3, 3, 8])
+    context 'boolean operations' do
+      it 'can return an array of booleans' do
+        expect(arr.mymap_recursive do |number|
+          number % 2 == 0
+        end).to eq([true, true, false, false, true])
+      end
     end
 
-    it 'can run multiplication on numbers' do
-      expect(arr.mymap_recursive { |number| number * 2 }).to eq([4, 4, 10, 10, 20])
-    end
+    context 'string operations' do
+      let(:strings) { %w('hello', 'i', 'am', 'pilgrim') }
 
-    it 'can run division on numbers' do
-      expect(arr.mymap_recursive { |number| number / 2 }).to eq([1, 1, 2, 2, 5])
-    end
-  end
-
-  context 'boolean operations' do
-    it 'can return an array of booleans' do
-      expect(arr.mymap_recursive do |number|
-        number % 2 == 0
-      end).to eq([true, true, false, false, true])
-    end
-  end
-
-  context 'string operations' do
-    let(:strings) { %w('hello', 'i', 'am', 'pilgrim') }
-
-    it 'can run any string method on strings' do
-      expect(strings.mymap_recursive(&:upcase)).to eq(%w('HELLO', 'I', 'AM', 'PILGRIM'))
+      it 'can run any string method on strings' do
+        expect(strings.mymap_recursive(&:upcase)).to eq(%w('HELLO', 'I', 'AM', 'PILGRIM'))
+      end
     end
   end
 end
